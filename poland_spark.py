a2cc1c2a44dc232c1f0aed914c79a19ade8cb72a
Harutyun Khachatryan <kh_harut@yahoo.com>
2024-08-21 16:50:32 +0200

    python spark code to select Poland data

---
+++ poland_spark.py
@@ -0,0 +1,11 @@
+from pyspark.sql import SparkSession
+import pandas as pd
+
+spark = SparkSession.builder.master("local[*]").appName("biodiversity").getOrCreate()
+
+occurence_df = spark.read.option("delimiter", ",").option("header", True).csv("occurence.csv")
+occurence_df_pol = occurence_df.filter("countryCode = 'PL'")
+multimedia_df = spark.read.option("delimiter", ",").option("header", True).csv("multimedia.csv")
+final_df = occurence_df_pol.join(multimedia_df,multimedia_df["CoreId"]==occurence_df_pol["id"],how="left")
+occur_pd_df = final_df.toPandas()
+occur_pd_df.to_csv("poland_biodiversity.csv", index=False, sep=",")
